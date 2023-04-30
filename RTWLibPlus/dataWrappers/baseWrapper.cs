98a49340f62eed9357c055958a57e6c8df35c039
Aaron T <aaronbaron@hotmail.co.uk>
2023-04-30 13:59:24 +0100

    baseWrapper: get items by criteria method, further test for Descr_strat utilising this

--- RTWLibPlus/dataWrappers/baseWrapper.cs
+++ RTWLibPlus/dataWrappers/baseWrapper.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace RTWLibPlus.dataWrappers
@@ -12,38 +13,14 @@ namespace RTWLibPlus.dataWrappers
     {
         public List<IbaseObj> data = new List<IbaseObj>();
         /// <summary>
-        /// Location is a collection of strings that represent the tags. Once the final string is found it will return the coressponding kv
+        /// Location is a collection of strings that represent the tags. Once the final string is found it will return the corresponding kv
         /// </summary>
         /// <param name="location"></param>
-        public KeyValuePair<string, string> GetKeyValueAtLocation(List<IbaseObj> items, int locInd = 0, params string[] location)
-        {
-            string tag = string.Empty;
-            string value = string.Empty;
-            for (int i = 0; i < items.Count; i++)
-            {
-                tag = ((baseObj)items[i]).Tag;
-                value = ((baseObj)items[i]).Value;
-
-                if (location[locInd] == ((baseObj)items[i]).Tag)
-                {
-                    if (locInd == location.Count() - 1)
-                        return new KeyValuePair<string, string>(tag, value);
-                    else return GetKeyValueAtLocation(items[i].GetItems(), ++locInd, location);
-                }
-            }
-            return new KeyValuePair<string, string>();
-        }
-
         public bool ModifyValue(List<IbaseObj> items, string newValue, int locInd = 0, bool done = false, params string[] location)
         {
-            string tag = string.Empty;
-            string value = string.Empty; 
-
             for (int i = 0; i < items.Count; i++)
             {
                 baseObj item = (baseObj)items[i];
-                tag = item.Tag;
-                value = item.Value;
 
                 if (location[locInd] == item.Tag)
                 {
@@ -57,7 +34,64 @@ namespace RTWLibPlus.dataWrappers
             }
             return done;
         }
+        public void DeleteValue(List<IbaseObj> items, string ident, int locInd = 0)
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                baseObj item = (baseObj)items[i];
 
+                if(ident == item.Ident)
+                {
+                    items.RemoveAt(i);
+                    i--;
+                    continue;
+                }
+
+                if (item.GetItems().Count > 0)
+                {
+                    ModifyValue(item.GetItems(), ident, ++locInd);
+                }
+            }
+        }
+        public bool AddObjToList(List<IbaseObj> items, IbaseObj obj, int locInd = 0, bool done = false, params string[] location)
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                baseObj item = (baseObj)items[i];
+
+                if (location[locInd] == item.Tag)
+                {
+                    if (locInd == location.Count() - 1)
+                    {
+                        item.GetItems().Add(obj);
+                        done = true;
+                    }
+                    else done = AddObjToList(item.GetItems(), obj, ++locInd, done, location);
+                }
+            }
+            return done;
+        }
+        public bool InsertNewObjectByCriteria(List<IbaseObj> items, IbaseObj obj, params string[] criteriaTags)
+        {
+            bool[] criteria = new bool[criteriaTags.Count()];
+            int currCrit = 0;
+            for (int i = 0; i < items.Count; i++)
+            {
+                baseObj item = (baseObj)items[i];
+                if (item.Tag == criteriaTags[currCrit] || item.Ident == criteriaTags[currCrit])
+                {
+                    criteria[currCrit] = true;
+                    currCrit++;
+                }
+
+                if (criteria.All((x) => x == true))
+                {
+                    items.Insert(i+1, obj);
+                    return true;
+                }
+            }
+            return false;
+        }
         public List<IbaseObj> GetItemsByIdent(string ident) {
             List<IbaseObj> found = new List<IbaseObj>();
             foreach(var item in data)
@@ -70,24 +104,61 @@ namespace RTWLibPlus.dataWrappers
             }
             return found;
         }
+        public List<IbaseObj> GetItemsByCriteria(string stopAt, string lookFor, params string[] criteriaTags)
+        {
+            bool[] criteria = new bool[criteriaTags.Count()];
+            int currCrit = 0;
+            List<IbaseObj> found = new List<IbaseObj>();
+            foreach (var item in data)
+            {
+                baseObj bi = (baseObj)item;
+                if (bi.Ident == criteriaTags[currCrit] || bi.Tag == criteriaTags[currCrit])
+                {
+                    criteria[currCrit] = true;
+                    if(currCrit != criteriaTags.Count() -1)
+                        currCrit++;
+                }
 
+                if (criteria.All((x) => x == true) && bi.Ident == lookFor)
+                {
+                    found.Add(bi) ;
+                }
+                if (stopAt == bi.Ident && criteria.All((x) => x == true))
+                    return found;
+            }
+            return found;
+        }
         public List<IbaseObj> GetItemList(List<IbaseObj> items, int locInd = 0, params string[] location)
         {
             string tag = string.Empty;
             string value = string.Empty;
             for (int i = 0; i < items.Count; i++)
             {
-                tag = ((baseObj)items[i]).Tag;
-                value = ((baseObj)items[i]).Value;
-
-                if (location[locInd] == ((baseObj)items[i]).Tag)
+                baseObj item = (baseObj)items[i];
+                
+                if (location[locInd] == item.Tag || location[locInd] == item.Ident)
                 {
                     if (locInd == location.Count() - 1)
-                        return items[i].GetItems();
-                    else return GetItemList(items[i].GetItems(), ++locInd, location);
+                        return item.GetItems();
+                    else return GetItemList(item.GetItems(), ++locInd, location);
                 }
             }
             return null;
+        }
+        public KeyValuePair<string, string> GetKeyValueAtLocation(List<IbaseObj> items, int locInd = 0, params string[] location)
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                baseObj item = (baseObj)items[i];
+
+                if (location[locInd] == item.Tag || location[locInd] == item.Ident)
+                {
+                    if (locInd == location.Count() - 1)
+                        return new KeyValuePair<string, string>(item.Tag, item.Value);
+                    else return GetKeyValueAtLocation(item.GetItems(), ++locInd, location);
+                }
+            }
+            return new KeyValuePair<string, string>();
         }
     }
 }
--- RTWLib_Tests/wrappers/Tests_ds.cs
+++ RTWLib_Tests/wrappers/Tests_ds.cs
@@ -48,10 +48,101 @@ namespace RTWLib_Tests.wrappers
             var parsedds = new DS(dsParse);
 
             var result = parsedds.GetItemsByIdent("resource");
-            var expected = 300; //number of settlements
+            var expected = 300; //number of resources
+
+            Assert.AreEqual(expected, result.Count); //check number of returned resources
+        }
+        [TestMethod]
+        public void dsGetItemsByIdentFaction()
+        {
+            var ds = TokenParse.ReadFile(RFH.CurrDirPath("resources", "descr_strat.txt"), false);
+            var dsParse = DepthParse.Parse(ds, Creator.DScreator);
+            var parsedds = new DS(dsParse);
+
+            var result = parsedds.GetItemsByIdent("faction");
+            var expected = 21; //number of factions
+
+            Assert.AreEqual(expected, result.Count); //check number of returned factions
+        }
+        [TestMethod]
+        public void dsGetItemsByIdentCoreAttitudes()
+        {
+            var ds = TokenParse.ReadFile(RFH.CurrDirPath("resources", "descr_strat.txt"), false);
+            var dsParse = DepthParse.Parse(ds, Creator.DScreator);
+            var parsedds = new DS(dsParse);
+
+            var result = parsedds.GetItemsByIdent("core_attitudes");
+            var expected = 47; //number of ca
+
+            Assert.AreEqual(expected, result.Count); //check number of returned ca
+        }
+        [TestMethod]
+        public void dsDeleteByIdent()
+        {
+            var ds = TokenParse.ReadFile(RFH.CurrDirPath("resources", "descr_strat.txt"), false);
+            var dsParse = DepthParse.Parse(ds, Creator.DScreator);
+            var parsedds = new DS(dsParse);
+
+            parsedds.DeleteValue(parsedds.data, "settlement");
+            var result = parsedds.GetItemsByIdent("settlement");
+            var expected = 0; //number of ca
+
+            Assert.AreEqual(expected, result.Count); //check number of returned ca
+        }
+        [TestMethod]
+        public void dsAddSettlementToRomansBrutii()
+        {
+            var ds = TokenParse.ReadFile(RFH.CurrDirPath("resources", "descr_strat.txt"), false);
+            var dsParse = DepthParse.Parse(ds, Creator.DScreator);
+            var parsedds = new DS(dsParse);
+
+            var settlements = parsedds.GetItemsByIdent("settlement");
+            var add = parsedds.InsertNewObjectByCriteria(parsedds.data, settlements[30], "faction\tromans_brutii,", "denari");
+            var result = parsedds.GetItemsByCriteria("character", "settlement", "faction\tromans_brutii,");
+            var expected = 3; //number of ca
+            RFH.Write("./dsresult.txt", parsedds.Output());
+            Assert.AreEqual(expected, result.Count); //check number of returned ca
+        }
+        [TestMethod]
+        public void dsAddSettlementToScythia()
+        {
+            var ds = TokenParse.ReadFile(RFH.CurrDirPath("resources", "descr_strat.txt"), false);
+            var dsParse = DepthParse.Parse(ds, Creator.DScreator);
+            var parsedds = new DS(dsParse);
+             
+            var settlements = parsedds.GetItemsByIdent("settlement");
+            var add = parsedds.InsertNewObjectByCriteria(parsedds.data, settlements[30], "faction\tscythia,", "denari");
+            var result = parsedds.GetItemsByCriteria("character", "settlement", "faction\tscythia,");
+            var expected = 5; //number of ca
+            RFH.Write("./dsresult.txt", parsedds.Output());
+            Assert.AreEqual(expected, result.Count); //check number of returned ca
+        }
+        [TestMethod]
+        public void dsAddUnitToFlavius()
+        {
+            var ds = TokenParse.ReadFile(RFH.CurrDirPath("resources", "descr_strat.txt"), false);
+            var dsParse = DepthParse.Parse(ds, Creator.DScreator);
+            var parsedds = new DS(dsParse);
+            var units = parsedds.GetItemsByCriteria("character", "unit", "faction\tromans_julii,", "character", "army");
+            var add = parsedds.InsertNewObjectByCriteria(parsedds.data, units[1] , "faction\tromans_julii,", "character\tFlavius", "unit");
+            var result = parsedds.GetItemsByCriteria("character", "unit", "character\tFlavius", "army");
+            var expected = 6; //number of ca
+            RFH.Write("./dsresult.txt", parsedds.Output());
+            Assert.AreEqual(expected, result.Count); //check number of returned ca
+        }
+
+        /*public void dsAddSettlement()
+        {
+            var ds = TokenParse.ReadFile(RFH.CurrDirPath("resources", "descr_strat.txt"), false);
+            var dsParse = DepthParse.Parse(ds, Creator.DScreator);
+            var parsedds = new DS(dsParse);
+
+
+            var result = parsedds.GetItemsByIdent("settlement");
+            var expected = 96; //number of settlements
 
             Assert.AreEqual(expected, result.Count); //check number of returned settlements
-        }
+        }*/
 
         /*[TestMethod]
         public void dsWholeFile()
