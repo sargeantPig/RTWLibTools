7f753b3eb4ab7a3d511b5a25b5c01d6f23a4ea3b
Aaron <aaronbaron@hotmail.co.uk>
2023-12-31 15:29:26 +0000

    add tests

--- RTWLib_Tests/modifiers/Tests_stratMod.cs
+++ RTWLib_Tests/modifiers/Tests_stratMod.cs
@@ -11,18 +11,20 @@ using RTWLibPlus.parsers.objects;
 using RTWLibPlus.Modifiers;
 
 [TestClass]
-public class Tests_ds
+public class TestsStrat
 {
     private readonly DepthParse dp = new();
     private readonly TWConfig config = TWConfig.LoadConfig(@"resources/remaster.json");
-    public void GetFactionByRegion()
+
+    [TestMethod]
+    public void ChangeRegionName()
     {
         string[] ds = this.dp.ReadFile(RFH.CurrDirPath("resources", "descr_strat.txt"), false);
         List<IBaseObj> dsParse = this.dp.Parse(ds, Creator.DScreator);
         DS parsedds = new(dsParse, this.config);
         List<IBaseObj> settlements = parsedds.GetItemsByIdent("settlement").DeepCopy();
         IBaseObj modifiedSettlement = StratModifier.CreateSettlement(settlements[0], "test_name");
-
-        Assert.AreEqual("test_name", modifiedSettlement.Find("region"));
+        string result = modifiedSettlement.Find("region");
+        Assert.AreEqual("test_name", result);
     }
 }
