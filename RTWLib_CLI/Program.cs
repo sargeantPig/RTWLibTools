8e26bdc21be770662e7f51faebdd404cd1a5444f
Aaron <aaronbaron@hotmail.co.uk>
2024-01-02 14:08:01 +0000

    fix: updated dotnet version to 8.0, supressed warning in editorconfig

--- RTWLib_CLI/Program.cs
+++ RTWLib_CLI/Program.cs
@@ -1,4 +1,5 @@
-﻿using RTWLib_CLI;
+﻿namespace RTWLIB_CLI;
+using RTWLib_CLI;
 using RTWLib_CLI.cmd;
 using RTWLib_CLI.cmd.modules;
 using RTWLib_CLI.draw;
@@ -7,43 +8,41 @@ using RTWLibPlus.data;
 using RTWLibPlus.helpers;
 using System;
 using System.IO;
-using System.Reflection;
-using System.Runtime.CompilerServices;
 
-namespace RTWLIB_CLI
+internal class Program
 {
-    class Program
+    private static readonly string Title = "Welcome to the RTWLib CLI\n     By Sargeant Pig\n---\ntype 'help' for commands and usage";
+    private static readonly string ConfigTitle = string.Format("Please select a config from below using the number{0}{1}",
+            "\n", CMDProcess.configs.DictToString());
+
+    private static void Main(string[] args)
     {
-        private static string title = "Welcome to the RTWLib CLI\n     By Sargeant Pig\n---\ntype 'help' for commands and usage";
-        private static string configTitle = string.Format("Please select a config from below using the number{0}{1}",
-                "\n", CMDProcess.configs.DictToString());
+        string wdir = AppDomain.CurrentDomain.BaseDirectory;
+        Directory.SetCurrentDirectory(wdir);
 
-        static void Main(string[] args)
+        Console.WriteLine(CMDProcess.LoadTemplates());
+        Console.WriteLine(CMDProcess.LoadConfigs());
+
+        if (CMDProcess.configs.Count == 0)
         {
-            string wdir = AppDomain.CurrentDomain.BaseDirectory;
-            Directory.SetCurrentDirectory(wdir);
-
-            Console.WriteLine(CMDProcess.LoadTemplates());
-            Console.WriteLine(CMDProcess.LoadConfigs());
-
-            if (CMDProcess.configs.Count == 0)
-                Environment.Exit(1);
-
-            CLIHelper.ScreenChange(title.ApplyBorder('#', 2, 1));
-            int input = Input.GetIntInput(configTitle, x => x >= 0 && x < CMDProcess.configs.Count);
-            TWConfig config = TWConfig.LoadConfig(CMDProcess.configs[input]);
-            RandCMD rand = new RandCMD(config);
-            Console.WriteLine("Config Loaded".ApplyBorder('#', 1, 1));
-            CMDProcess.modules.RegisterModule(rand);
-            //Rand.InitialSetup();
-            while (true)
-            {
-                string ret = CMDProcess.ReadCMD(Console.ReadLine());
-
-                if (ret != KW.back) { Console.WriteLine(ret.ApplyBorder('=', 1, 1)); continue; }
-                CLIHelper.ScreenChange(title);
-            }
+            Environment.Exit(1);
         }
 
+        CLIHelper.ScreenChange(Title.ApplyBorder('#', 2, 1));
+        int input = Input.GetIntInput(ConfigTitle, x => x >= 0 && x < CMDProcess.configs.Count);
+        TWConfig config = TWConfig.LoadConfig(CMDProcess.configs[input]);
+        RandCMD rand = new(config);
+        Console.WriteLine("Config Loaded".ApplyBorder('#', 1, 1));
+        CMDProcess.modules.RegisterModule(rand);
+        //Rand.InitialSetup();
+        while (true)
+        {
+            string ret = CMDProcess.ReadCMD(Console.ReadLine());
+
+            if (ret != KW.back)
+            { Console.WriteLine(ret.ApplyBorder('=', 1, 1)); continue; }
+            CLIHelper.ScreenChange(Title);
+        }
     }
+
 }
